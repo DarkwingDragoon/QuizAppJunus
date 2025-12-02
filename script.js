613581f38d50f7dd3802d08857e0a5e68ee88e8c
DarkwingDragoon <darkwingdragoon@gmail.com>
2025-12-02 20:49:09 +0100

    add 10 questions in js asl JSON  questions

---
+++ script.js
@@ -0,0 +1,82 @@
+let questions = [
+  {
+    "question": "Was bedeutet das Akronym SQL?",
+    "answer_1": "Structured Query Language",
+    "answer_2": "Simple Query Layer",
+    "answer_3": "System Quality Language",
+    "answer_4": "Source Query Logic",
+    "right_answer": "Structured Query Language"
+  },
+  {
+    "question": "Welche Datenstruktur arbeitet nach dem LIFO-Prinzip?",
+    "answer_1": "Stack",
+    "answer_2": "Queue",
+    "answer_3": "Array",
+    "answer_4": "Linked List",
+    "right_answer": "Stack"
+  },
+  {
+    "question": "Welche Schleife wird meist genutzt, um eine feste Anzahl an Durchläufen zu wiederholen?",
+    "answer_1": "for-Schleife",
+    "answer_2": "while-Schleife",
+    "answer_3": "do-while-Schleife",
+    "answer_4": "repeat-until-Schleife",
+    "right_answer": "for-Schleife"
+  },
+  {
+    "question": "Welches der folgenden ist KEIN primitiver Datentyp in vielen Programmiersprachen?",
+    "answer_1": "String",
+    "answer_2": "Integer",
+    "answer_3": "Boolean",
+    "answer_4": "Float",
+    "right_answer": "String"
+  },
+  {
+    "question": "Welche Datei-Endung wird typischerweise für JavaScript-Dateien verwendet?",
+    "answer_1": ".js",
+    "answer_2": ".java",
+    "answer_3": ".jsx",
+    "answer_4": ".script",
+    "right_answer": ".js"
+  },
+  {
+    "question": "Welches Schlüsselwort wird in Java verwendet, um eine Klasse zu erben?",
+    "answer_1": "extends",
+    "answer_2": "inherits",
+    "answer_3": "super",
+    "answer_4": "implement",
+    "right_answer": "extends"
+  },
+  {
+    "question": "Welche Sprache wird hauptsächlich zur Gestaltung von Webseiten genutzt?",
+    "answer_1": "CSS",
+    "answer_2": "Python",
+    "answer_3": "C++",
+    "answer_4": "SQL",
+    "right_answer": "CSS"
+  },
+  {
+    "question": "Welches Symbol wird in vielen Programmiersprachen für Kommentare genutzt?",
+    "answer_1": "//",
+    "answer_2": "##",
+    "answer_3": "--",
+    "answer_4": "\\\\",
+    "right_answer": "//"
+  },
+  {
+    "question": "Was bedeutet das Konzept 'OOP'?",
+    "answer_1": "Object-Oriented Programming",
+    "answer_2": "Open Operations Protocol",
+    "answer_3": "Overload-Oriented Process",
+    "answer_4": "Object-Optimization Procedure",
+    "right_answer": "Object-Oriented Programming"
+  },
+  {
+    "question": "Welche dieser Sprachen ist keine Programmiersprache?",
+    "answer_1": "HTML",
+    "answer_2": "Python",
+    "answer_3": "Ruby",
+    "answer_4": "C#",
+    "right_answer": "HTML"
+  }
+];
